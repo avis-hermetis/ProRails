c11f8415b9a81c67627741d73f72af57c3d8f7c6
Michael <avis-hermetis@yandex.ru>
2015-11-12 23:35:31 +1000

    add dependent: :destroy

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -52,13 +52,6 @@ RSpec.configure do |config|
 end
 
 Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    # Choose a test framework:
-    with.test_framework :rspec
-    with.library :active_record
-    with.library :active_model
-    with.library :action_controller
-    # Or, choose all of the above:
-    with.library :rails
-  end
-end
+  config.integrate do |with| with.test_framework :rspec
+  with.library :active_record
+  with.library :rails end end
